8360c4c0e501b24a3140e82af375d9e493923f34
Gustavo Monti Rocha <gustavo.rocha@intelliway.com.br>
2024-11-25 15:48:45 -0300

    ok4

--- app/api/verify-payment/route.ts
+++ app/api/verify-payment/route.ts
@@ -3,12 +3,14 @@ import Stripe from 'stripe';
 import { PrismaClient } from '@prisma/client'
 
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-11-20.acacia',
-});
-
 export async function POST(req: Request) {
   const prisma = new PrismaClient();
+
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2024-11-20.acacia',
+  });
+  
   try {
     const { sessionId, userId } = await req.json();
     console.log(sessionId, userId)
