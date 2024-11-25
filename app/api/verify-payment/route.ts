ffcdf4f07b318bc7b8cfb19cc5076c531b209685
Gustavo Monti Rocha <gustavo.rocha@intelliway.com.br>
2024-11-25 16:09:33 -0300

    ok5

--- app/api/verify-payment/route.ts
+++ app/api/verify-payment/route.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client'
 
 if (!process.env.STRIPE_SECRET_KEY) throw new Error("no key")
   export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-      apiVersion: '2024-11-20.acacia',
+      apiVersion: '2024-06-20',
       typescript: true
   })
   export { Stripe };
