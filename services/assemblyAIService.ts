7a080c7610bea4feba5098fd1acb1c240977cffc
Gustavo Monti Rocha <gustavo.rocha@intelliway.com.br>
2024-11-27 11:11:23 -0300

    assemblyai api not public1

--- services/assemblyAIService.ts
+++ services/assemblyAIService.ts
@@ -2,7 +2,7 @@
 
 import { AssemblyAI } from 'assemblyai'; // Ensure you have the correct import for AssemblyAI
 
-export const createAssemblyAIClient = () => {
+export const createAssemblyAIClient = async () => {
   return new AssemblyAI({
     apiKey: process.env.ASSEMBLYAI_API_KEY || '',
   });
